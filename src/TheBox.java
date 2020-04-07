b1babf8a92006891c8b014d658126e9f5a412f71
Rainbovv <63318406+Rainbovv@users.noreply.github.com>
2020-04-07 23:46:06 +0300

    Add files via upload

--- src/TheBox.java
+++ src/TheBox.java
@@ -8,43 +8,43 @@ class TheBox {
 
 
 	  public static void main(String[] args) {
-	      Scanner in = new Scanner(System.in);
+		  Scanner in = new Scanner(System.in);
 
 	//# Variables
 
-	    int value, width, height, lenght, volume;  
+		  int value, width, height, lenght, volume;  
 
 
 	/////////////// INPUT LAYER //////////////////////
-	    System.out.println("Enter the VALUE of the BOX:");
-	    value = in.nextInt();
+		  System.out.println("Enter the VALUE of the BOX:");
+		  value = in.nextInt();
 	    
-	    System.out.printf("%nThe BOX is OK or BROKEN?%nIf OK %6s enter %2s  1%nIf BROKEN %2s enter %2s  2%n","-","-","-","-");
-	    Box.isTheBoxBroken();
-	    
-	    System.out.println("\nEnter the LENGHT of the BOX:");
-	    lenght = in.nextInt();
-	    
-	    System.out.println("\nEnter the WIDTH of the BOX:");
-	    width = in.nextInt();
-	    
-	    System.out.println("\nEnter the HEIGHT of the BOX:");
-	    height = in.nextInt();
-
-	    volume = lenght * width * height;
+		  System.out.printf("%nThe BOX is OK or BROKEN?%nIf OK %6s enter %2s  1%nIf BROKEN %2s enter %2s  2%n","-","-","-","-");
+		  Box.isTheBoxBroken();
+		    
+		  System.out.println("\nEnter the LENGHT of the BOX:");
+		  lenght = in.nextInt();
+		    
+		  System.out.println("\nEnter the WIDTH of the BOX:");
+		  width = in.nextInt()  ;
+		    
+		  System.out.println("\nEnter the HEIGHT of the BOX:");
+		  height = in.nextInt();
+	
+		  volume = lenght * width * height;      
 
 	/////////////// OUTPUT /////////////
-		divider();
-	    Box.print(value);
-	    Box.printIsTheBoxBig(value);
-		Box.printIsTheBoxOk();
-	    Box.printBoxSize(lenght, width, height);
-	    Box.printBoxVolume(volume);
+		  divider();
+		  Box.print(value);
+		  Box.printIsTheBoxBig(value);
+		  Box.printIsTheBoxOk();
+		  Box.printBoxSize(lenght, width, height);
+		  Box.printBoxVolume(volume);
 
 	  }
 	  
 	  static void divider(){
-	    System.out.println("\n#########################\n");
+		  System.out.println("\n#########################\n");
 	  }
 }
 	///////////////////////
@@ -56,7 +56,8 @@ class Box{
 	/// Checking the status of the BOX.
 	  static void isTheBoxBroken(){
 		  Scanner in = new Scanner(System.in);
-	      switch(in.nextInt()){
+	      
+		  switch(in.nextInt()){
 	      case 1:
 	    	  broken = false;
 	    	  break;
@@ -67,28 +68,28 @@ class Box{
 	    	  System.out.println("Error, plz enter a valid option!");
 	    	  isTheBoxBroken();
 	      }
-	    }
+	  }
 
 	//# Parameters of output!
 	  static void print(int value){
-			System.out.println( "This is a BOX = " + value );
-		}
+		  System.out.println( "This is a BOX = " + value );
+	  }
 		
 	  static void printIsTheBoxBig(int value){
-			if( value > 1000 ){
-				System.out.println( "This is a BIG BOX" );
-			}else{
-				System.out.println( "This is a SMALL BOX" );
-			}
-		}
+		  if( value > 1000 ){
+			  System.out.println( "This is a BIG BOX" );
+		  }else{
+			  System.out.println( "This is a SMALL BOX" );
+		  }
+	  }
 		
 	  static void printIsTheBoxOk(){
-			if( broken ){
-				System.out.println( "The BOX is BROKEN!" );
-			}else{
-				System.out.println( "The BOX is OK!" );
-			}
-		}
+		  if( broken ){
+			  System.out.println( "The BOX is BROKEN!" );
+		  }else{
+			  System.out.println( "The BOX is OK!" );
+		  }
+	  }
 	  
 	  static void printBoxSize(int lenght, int width, int height){
 		  System.out.println("The size of the BOX is: " + lenght + "cm x " + width + "cm x " + height + "cm." );
